1c9c6a125a3fc9bdd86ae532fa4bff91b2d1a7da
suryansh030 <suryanshverma435@gmail.com>
2025-02-19 02:16:13 +0530

    .

--- binary_search.c
+++ binary_search.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
-int binarySearch(int arr[], int size, int number) {
-    int left = 0;
-    int right = size - 1;
 
-    while (left <= right) {
-        int mid = left + (right - left) / 2; 
+
+int binarySearch(int arr[], int size, int number) {
+    int l = 0;
+    int r = size - 1;
+
+    while (l <= r) {
+        int mid = l + (r - l) / 2; 
         if (arr[mid] == number) {
             return mid;
         }
 
         if (arr[mid] < number) {
-            left = mid + 1;
+            l = mid + 1;
         } 
         else {
-            right = mid - 1;
+            r = mid - 1;
         }
     }
     return -1;
 }
 
 int main() {
-    int arr[] = {0, 3, 5, 9, 10, 19, 23};
+    int arr[] = {2, 4, 6, 8, 12, 16, 20};
     int size = sizeof(arr) / sizeof(arr[0]);
     int number;
 
@@ -30,10 +32,51 @@ int main() {
     int result = binarySearch(arr, size, number);
 
     if (result != -1) {
-        printf("Element is present at index %d.\n", result);
+        printf("Element is at index %d.\n", result);
     } else {
-        printf("Element is not present in the array.\n");
+        printf("Element is not present.\n");
     }
 
     return 0;
-}   
+}
+#include <stdio.h>
+
+
+int binarySearch(int arr[], int size, int number) {
+    int l = 0;
+    int r = size - 1;
+
+    while (l <= r) {
+        int mid = l + (r - l) / 2; 
+        if (arr[mid] == number) {
+            return mid;
+        }
+
+        if (arr[mid] < number) {
+            l = mid + 1;
+        } 
+        else {
+            r = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 12, 16, 20};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int number;
+
+    printf("Enter the number to search: ");
+    scanf("%d", &number);
+
+    int result = binarySearch(arr, size, number);
+
+    if (result != -1) {
+        printf("Element is at index %d.\n", result);
+    } else {
+        printf("Element is not present.\n");
+    }
+
+    return 0;
+}
